cb91ab4ffca1490c88201f7a22219bcc8662b761
Derek Anthony Rios Ramirez <a1263922@uabc.edu.mx>
2022-10-12 22:48:35 -0700

    COMMIT1 - Se agrego main y zen372

---
+++ zen372.py
@@ -0,0 +1,13 @@
+"""
+Comentario de varias lineas.
+"""
+
+# Comentario de unas sola linea.
+
+print("Latosa1: nose.")
+print("Latosa2: mas vale pedir perdon que pedir permiso.")
+print("Marco1: camaron que se duerme bla bla bla.")
+print("Marco2: fuga")
+print("Derek: fugax2")
+
+barba = "Algo, que sorpresa!!!"
